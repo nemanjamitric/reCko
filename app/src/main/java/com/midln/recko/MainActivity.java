ed3e304b2d935c84588f724f70f1ec46fca592e6
Nemanja Mitric <nemanjamitric01@gmail.com>
2022-12-03 17:09:20 +0100

    Kreiranje play i navigiranje na njega

--- app/src/main/java/com/midln/recko/MainActivity.java
+++ app/src/main/java/com/midln/recko/MainActivity.java
@@ -2,15 +2,30 @@ package com.midln.recko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button pocniBtn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pocniBtn = findViewById(R.id.pocniBtn);
+        pocniBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
     }
 }
