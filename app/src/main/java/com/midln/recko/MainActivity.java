a812e15fdf9e672882155a6669e39b1f73f082d9
Mihajlo Zigic <mihalozigalo@gmail.com>
2022-12-18 15:30:07 +0100

    Added put method to jsop bin api

--- app/src/main/java/com/midln/recko/MainActivity.java
+++ app/src/main/java/com/midln/recko/MainActivity.java
@@ -7,26 +7,31 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressDialog pd;
-
+    WordsObject wordsGlobal = new WordsObject();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +40,12 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(getApplicationContext(), "Cao", Toast.LENGTH_LONG);
         toast.show();
         //json call
-        new JsonTask().execute("https://api.jsonbin.io/v3/b/638b6b14003d6444ce61ea62/");
+        new GetWords().execute("https://api.jsonbin.io/v3/b/638b6b14003d6444ce61ea62/");
         //end of json call
     }
 
     //json functions
-    private class JsonTask extends AsyncTask<String, String, String> {
+    private class GetWords extends AsyncTask<String, String, String> {
 
         protected void onPreExecute() {
             super.onPreExecute();
@@ -128,10 +133,80 @@ public class MainActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
+            //show data in toast
             Toast toast = Toast.makeText(getApplicationContext(), words.Words.get(0).WordEn, Toast.LENGTH_LONG);
             toast.show();
+            //save data for post
+            wordsGlobal = words;
+            new PushWords().execute("https://api.jsonbin.io/v3/b/638b6b14003d6444ce61ea62");
         }
     }
+
+    private class PushWords extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty ("X-Master-Key", "$2b$10$BPLdapvarWs/vuRwoTuoqOe0X6kGVmDMI/y9zyRDY7sRM9C1LSt/6");
+                connection.setRequestProperty ("X-Access-Key", "$2b$10$V6CKmezfWtmInV8l6MWvK.RZYAyQbNA6vKJpD2pbLDimvVk8IkBpi");
+                connection.setRequestMethod("PUT");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
+
+                //change words to add more
+                wordsGlobal.Version += 1;
+                wordsGlobal.Words.add(new Word("Cao","Hello", 1));
+
+
+                //push to json bin
+                OutputStreamWriter wr = new OutputStreamWriter (connection.getOutputStream());
+                wr.write(new Gson().toJson(wordsGlobal));
+                wr.flush();
+                wr.close();
+
+                //read response
+                return  Integer.toString(connection.getResponseCode());
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.e("TAG", result); // this is expecting a response code to be sent from your server upon receiving the POST data
+        }
+    }
+
     //end of json functions
 }
